6bc15318347f6321e93c13c4533cd39551cb1012
crazer <nikit.sigov@gmail.com>
2018-03-29 17:50:52 +0300

    balance

--- src/games/balance.js
+++ src/games/balance.js
@@ -1,6 +1,7 @@
 import { cons } from 'hexlet-pairs';
 import game from '..';
 import { getRandom } from '../helpers';
+import readlineSync from 'readline-sync';
 
 const rules = 'Balance the given number.';
 
@@ -11,8 +12,39 @@ const checkBalance = (unbalance) => {
 };
 
 const getBalance = (unbalance) => {
+  const iter = (cur, acc) => {
+    console.log(cur, acc);
+    let current = +acc[cur];
+    let next = +acc[cur + 1];
+    if (cur === unbalance.length - 1) {
+      console.log('дошли до конца');
+      if (checkBalance(acc.join(''))) {
+        console.log('итог');
+        return acc;
+      }
+      iter(0, acc);
+    }
+    if (current - next > 0) {
+      current -= 1;
+      acc[cur] = current;
+      next += 1;
+      acc[cur + 1] = next;
+      return iter(cur, acc);
+    } else if (current - next < -1) {
+      current += 1;
+      acc[cur] = current;
+      next -= 1;
+      acc[cur + 1] = next;
+      return iter(cur, acc);
+    }
+    const a = readlineSync.question("Ff");
+    return iter(cur + 1, acc);
+  };
 
+  return iter(0, unbalance.split(''));
 };
+
+console.log(getBalance('215'));
 
 const generate = () => {
   const getUnbalance = (num) => {
