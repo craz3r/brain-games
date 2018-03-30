65310f2aac1141e7cbc8ddee8b412b5b9399affa
crazer <nikit.sigov@gmail.com>
2018-03-30 13:39:14 +0300

    simplify gcd func

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,8 @@
 export const getRandom = (min, max) => Math.floor(Math.random() * ((max - min) + 1)) + min;
 
 export const getGCD = (a, b) => {
-  if (a < 0 && b < 0) return getGCD(-a, -b);
-  else if (a < 0) return getGCD(-a, b);
-  else if (b < 0) return (getGCD(a, -b));
-  if (b >= a) {
-    return b % a ? getGCD(a, b % a) : a;
+  if (b === 0) {
+    return Math.abs(a);
   }
-  return a % b ? getGCD(a % b, b) : b;
+  return getGCD(b, a % b);
 };
